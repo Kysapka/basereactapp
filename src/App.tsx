b949759cae6a05612d4319d5cb8a14f44331289c
Артем <kysapka@gmail.com>
2022-01-23 23:01:39 +0300

    decomposition components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React, {SyntheticEvent, useState} from 'react';
-import './styles/App.css';
-import {PostsList} from "./PostsList";
-import {MyButton} from "./UI/button/MyButton";
-import MyInput from "./UI/input/MyInput";
+import './components/styles/App.css';
+import {PostsList} from "./components/PostsList";
+import {MyButton} from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
+import {postType} from "./components/types";
 
 
 export const App = () => {
@@ -13,34 +15,13 @@ export const App = () => {
         {id: 3, title: 'JavaScript 3', body: 'Description'},
     ])
 
-    const [title, setTitle] = useState('')
-    const [body, setBody] = useState('')
-
-    const addNewPost = (e: SyntheticEvent) => {
-        e.preventDefault()
-        const newPost = {id: +new Date(), title: 'JavaScript 1', body}
+    const createPost = (newPost: postType) => {
         setPosts([...posts, newPost])
-        setTitle('')
-        setBody('')
     }
 
     return (
         <div className="App">
-            <form>
-                <MyInput
-                    value={title}
-                    onChange={(e: SyntheticEvent<HTMLInputElement>) => setTitle(e.currentTarget.value)}
-                    type="text"
-                    placeholder="Название поста"
-                />
-                <MyInput
-                    value={body}
-                    onChange={(e: SyntheticEvent<HTMLInputElement>) => setBody(e.currentTarget.value)}
-                    type="text"
-                    placeholder="Описание поста"
-                />
-                <MyButton onClick={addNewPost}>Создать пост</MyButton>
-            </form>
+            <PostForm create={createPost}/>
             <PostsList posts={posts} title={'Посты про JS'}/>
         </div>
     );
